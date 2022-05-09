75f643e5b96bacb70a5ca336f751fa8bf541edbc
George Tang <gtangg12@mit.edu>
2022-05-08 20:53:56 -0400

    experiment infra revamp

--- experiments/babyai_gpt2_finetune_num_data.py
+++ experiments/babyai_gpt2_finetune_num_data.py
@@ -1,32 +1,29 @@
 import os
 
-from experiment_utils import (
-    make_args,
-    make_sbatch,
-    LOGGING_DIR,
+from experiment_utils import Experiment, ExperimentArgs
+
+
+EXPERIMENT_NAME = 'babyai_gpt2_finetune_num_data'
+
+experiment_args = ExperimentArgs()
+
+experiment = Experiment(
+    experiment_name=EXPERIMENT_NAME,
+    dataset_name='babya/env_description_chunked',
+    slurm_spec='task_sequence_gpt2_finetune.sh',
+    args=experiment_args
 )
-
-
-EXPERIMENT_NAME = 'babyai_gpt2_finetune'
-
-BASE_DIR = '/nobackup/users/gtangg12/task_planning_bayes_factor'
-DATA_DIR = f'{BASE_DIR}/data/babyai/env_description_chunked'
-CHECKPOINTS_DIR = f'{BASE_DIR}/checkpoints'
-SLURM_SPEC = f'{BASE_DIR}/slurm/task_sequence_gpt2_finetune.sh'
-
 
 NUM_DATA = [100, 500, 1000, 2500, 5000, 10000, 50000, 100000]
 
 for n in NUM_DATA:
     run_name = f'{EXPERIMENT_NAME}_{n}'
-    sbatch_cmd = make_sbatch(f'{LOGGING_DIR}/{run_name}', SLURM_SPEC)
-    experiment_params = {
+    run_params = {
         'num_data': n,
-        'data_dir': DATA_DIR,
-        'output_dir': f'{CHECKPOINTS_DIR}/{run_name}',
+        'data_dir': experiment.data_dir,
+        'logging_dir': experiment.logging_dir,
+        'checkpoint_dir': experiment.checkpoints_dir,
     }
-    experiment_params = make_args(experiment_params)
-    cmd = f'{sbatch_cmd} {experiment_params}'
-    os.system(cmd)
+    experiment.run(run_name, run_params)
 
 
--- experiments/experiment_utils.py
+++ experiments/experiment_utils.py
@@ -2,8 +2,14 @@ import os
 from typing import Dict
 
 
-LOGGING_DIR = f'runs/'
-os.makedirs(LOGGING_DIR, exist_ok=True)
+# absolute directory of work ecosystem
+BASE_DIR = '/nobackup/users/gtangg12/task_planning_bayes_factor'
+
+# respective absolute directories for each module
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+SLURM_DIR = os.path.join(BASE_DIR, 'slurm')
+CHECKPOINTS_DIR = os.path.join(BASE_DIR, 'checkpoints')
+EXPERIMENTS_DIR = os.path.join(BASE_DIR, 'experiments')
 
 
 def wrap_quotes(str: str):
@@ -18,12 +24,41 @@ def make_args(args: Dict) -> str:
     return wrap_quotes(' '.join(cmd))
 
 
-def make_sbatch(run_name: str, slurm_path: str) -> str:
+def make_sbatch(run_name: str, logging_dir: str, slurm_path: str) -> str:
     cmd = [
         'sbatch',
         f'-J {run_name}',
-        f'-o {run_name}.out',
-        f'-e {run_name}.err',
+        f'-o {logging_dir}/{run_name}.out',
+        f'-e {logging_dir}/{run_name}.err',
         slurm_path,
     ]
     return ' '.join(cmd)
+
+
+class ExperimentArgs:
+    pass
+    # TODO gpus cpus etc pipelines
+
+
+class Experiment:
+    def __init__(
+        self, 
+        experiment_name: str, 
+        dataset_name: str, 
+        slurm_spec: str, 
+        args: ExperimentArgs
+    ) -> None:
+        self.experiment_name = experiment_name
+        self.args = args
+        os.makedirs(experiment_name, exist_ok=True)
+
+        self.data_dir = os.path.join(DATA_DIR, dataset_name)
+        self.logging_dir = os.path.join(EXPERIMENTS_DIR, experiment_name)
+        self.checkpoints_dir = os.path.join(CHECKPOINTS_DIR, experiment_name)
+        self.slurm_spec_path = os.path.join(SLURM_DIR, slurm_spec)
+
+    def run(self, run_name, params):
+        sbatch_cmd = make_sbatch(run_name, self.logging_dir, self.slurm_spec_path)
+        params_cmd = make_args(params)
+        cmd = f'{sbatch_cmd} {params_cmd}'
+        os.system(cmd)
