f036fce2c5770b73462f1351806977a049d1e1b7
George Tang <gtangg12@mit.edu>
2022-06-01 14:06:44 -0400

    experiment utils

--- experiments/experiment_utils.py
+++ experiments/experiment_utils.py
@@ -1,45 +1,44 @@
-from typing import Dict, List, Set
+from typing import Dict, List, Iterable
 
 
 def list_of_dicts(n: int) -> List[Dict]:
-    ''' Helper function to make a list of n empty dicts '''
+    ''' Make a list of n empty dicts '''
     return [{} for _ in range(n)]
 
 
-def check_keys_consistent(params_list: List[Dict]) -> bool:
-    ''' Helper function to check if all params have the same keys. 
-        Assumes params_list has nonzero length. 
-    '''
-    keys = params_list[0].keys()
-    for params in params_list:
-        if params.keys() != keys:
+def check_keys_consistent(list_of_dicts: List[Dict]) -> bool:
+    ''' Check if all dicts in list have the same keys '''
+    if len(list_of_dicts) == 0:
+        return True
+    keys = list_of_dicts[0].keys()
+    for d in list_of_dicts:
+        if d.keys() != keys:
             return False
     return True
+    
+
+def check_has_keys(d: Dict, keys: Iterable) -> bool:
+    ''' Check if dict d contains keys in keys '''
+    if not isinstance(keys, set):
+        keys = set(keys)
+    return len(d.keys() & keys)
 
 
-def default_make_run_name(run_params: Dict, run_index: int) -> str:
-    ''' Helper function to make a run name from run param dict '''
-    if len(run_params) == 0:
+def default_make_run_name(params: Dict, run_index: int) -> str:
+    ''' Make default run name from params dict '''
+    if len(params) == 0:
         return f'run{run_index}'
     name = []
-    for key, value in run_params.items():
+    for key, value in params.items():
         name.append(f'{key}-{value}')
     return '_'.join(name)
 
 
-def make_args_command_sbatch(args: List) -> str:
-    ''' Helper function to make a string of sbatch arguments from args dict '''
-    cmd = []
-    for value in args:
-        cmd.append(f'--{value}')
-    return ' '.join(cmd)
-
-
-def make_args_command(args: Dict) -> str:
-    ''' Helper function to make a string of command line arguments from args dict '''
+def make_python_args_command(args: Dict) -> str:
+    ''' Make python command line arguments string from args dict '''
     cmd = []
     for key, value in args.items():
         cmd.append(f'--{key} {value}')
     cmd = ' '.join(cmd)
-    # wrap in quotes for bash to pass as args string to python
+    # wrap in quotes for bash to pass to python
     return f'"{cmd}"'
