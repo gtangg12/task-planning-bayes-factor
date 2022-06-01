08e80a9115f0036ea2d117af3b18e6c14609818c
George Tang <gtangg12@mit.edu>
2022-05-31 23:05:03 -0400

    move imports

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -1,5 +1,9 @@
 from workflows.trainer import Trainer
 from workflows.training_args import TrainingArguments
 
-from transformers import Trainer as TransformerTrainer
-from transformers import TrainingArguments as TransformerTrainingArguments
+from workflows.trainer_transformer import (
+    Trainer as TransformersTrainer,
+    TrainerCallback as TransformersTrainerCallback,
+    TrainingArguments as TransformersTrainingArguments,
+)
+from workflows.trainer_transformer import LoggingCallback as TransformersLoggingCallback
