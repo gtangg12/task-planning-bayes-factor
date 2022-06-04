d7631db1374f2c4db318eb7e78a6df035b1a485d
George Tang <gtangg12@mit.edu>
2022-06-04 11:47:00 -0400

    function name changes

--- experiments/experiment_utils.py
+++ experiments/experiment_utils.py
@@ -6,7 +6,7 @@ def list_of_dicts(n: int) -> List[Dict]:
     return [{} for _ in range(n)]
 
 
-def check_keys_consistent(list_of_dicts: List[Dict]) -> bool:
+def list_of_dicts_keys_consistent(list_of_dicts: List[Dict]) -> bool:
     ''' Check if all dicts in list have the same keys '''
     if len(list_of_dicts) == 0:
         return True
@@ -17,21 +17,11 @@ def check_keys_consistent(list_of_dicts: List[Dict]) -> bool:
     return True
     
 
-def check_has_keys(d: Dict, keys: Iterable) -> bool:
+def dict_has_keys(d: Dict, keys: Iterable) -> bool:
     ''' Check if dict d contains keys in keys '''
     if not isinstance(keys, set):
         keys = set(keys)
     return len(d.keys() & keys)
-
-
-def default_make_run_name(params: Dict, run_index: int) -> str:
-    ''' Make default run name from params dict '''
-    if len(params) == 0:
-        return f'run{run_index}'
-    name = []
-    for key, value in params.items():
-        name.append(f'{key}-{value}')
-    return '_'.join(name)
 
 
 def make_python_args_command(args: Dict) -> str:
