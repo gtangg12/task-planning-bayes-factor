4bac8c1b62a6a840f78b0453db7df47ed40e4fa8
George Tang <gtangg12@mit.edu>
2022-06-04 11:43:42 -0400

    logging utils

--- experiments/logging_utils.py
+++ experiments/logging_utils.py
@@ -2,16 +2,17 @@ import os
 import time
 
 
-def get_current_time_str() -> str:
+def current_time_str() -> str:
     ''' Return current time as string '''
     return time.strftime("%Y%m%d-%H%M%S")
     
 
-def default_make_log_filename(logging_dir: str) -> str:
+def default_log_filename(logging_dir: str, make_dir=True) -> str:
     ''' Return path of log with name current time located at logging_dir, creating
         logging_dir if necessary. 
     '''
-    os.makedirs(logging_dir, exist_ok=True)
-    current_time_str = get_current_time_str()
-    logging_filename = f'{logging_dir}/{current_time_str}.log'
+    if make_dir:
+        os.makedirs(logging_dir, exist_ok=True)
+    logging_filename = current_time_str()
+    logging_filename = f'{logging_dir}/{logging_filename}.log'
     return logging_filename
