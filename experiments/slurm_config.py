6be12d1d3ac84e3a6ddd46b6b87424a7448d39ab
George Tang <gtangg12@mit.edu>
2022-06-04 11:57:23 -0400

    var changes

--- experiments/slurm_config.py
+++ experiments/slurm_config.py
@@ -7,6 +7,3 @@ import os
 
 # maximum number of jobs allocated to user
 SLURM_MAX_CONCURRENT_RUNNING_JOBS = 4 
-
-# template file for running experiments
-SLURM_TEMPLATE_PATH = os.path.dirname(__file__) + '/template.sh' 
